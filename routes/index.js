e5ef26137a0bd15eed4026af8cbd03e914e7d177
Natanael A. de Morais <natanaelallves@gmail.com>
2021-12-19 11:42:39 -0300

    add icone do Catch-catch na aba e a tela de login

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,22 @@ router.get('/tutorial', function(req, res, next) {
   res.render('tutorial')
 });
 router.get('/sobre', function(req, res, next) {
-  // res.send('Em manutenção. (sobre)');
   res.render('sobre')
 });
+router.get('/login', function(req, res, next){
+  res.render('login')
+})
+router.post('/auth', function(req, res, next){
+  res.send(req.body)
+})
+router.get('/admin', function(req, res, next){
+  res.redirect('/login')
+})
+router.get('/admin/palavras', function(req, res, next){
+  res.redirect('/login')
+})
+router.get('/admin/palavras/nova', function(req, res, next){
+  res.redirect('/login')
+})
 
 module.exports = router;
