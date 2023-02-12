a29fa42393b7950fca5de1e348d27d1654914cc6
Natanael A. de Morais <natanaelallves@gmail.com>
2023-02-12 18:43:43 -0300

    configurar deploy na vercel

--- app.js
+++ app.js
@@ -16,30 +16,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-
-// const Sequelize = require('sequelize')
-// const sequelize = new Sequelize('catch_catch', 'root', '12345678', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-// })
-
-// models
-// const { User } = require('./app/models');
-
-// async function connect() {
-//   await sequelize.authenticate()
-//       .then(() => {
-//         console.log('Autenticado com sucesso!');
-//       })
-//       .catch(() => {
-//         console.log('Vish, deu bom não.');
-//       })
-// }
-// connect()
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
-
 module.exports = app;
